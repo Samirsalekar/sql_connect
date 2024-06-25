ac962d6b3309fc30fabbed36f55e5e4fc84a6429
Samirsalekar <162707298+Samirsalekar@users.noreply.github.com>
2024-06-25 19:11:47 +0530

    initial commit

---
+++ connecting sql.py
@@ -0,0 +1,12 @@
+# import mysql.connector
+#
+# my_con=mysql.connector.connect(host="localhost",user="root",password="Samsql@970",port="3306")
+# print(my_con)
+# # mydb = my_connection.cursor()
+# # mydb.execute("show databases")
+# # for i in mydb:
+# #     print(i)
+# # if (my_con):
+# #     print("connection successful")
+# # else:
+# #     print("connection unsuccessful")
